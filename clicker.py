a779679ca4e7f04c764cdc9edaea90d3f4c8fdce
Tyler Schoulte <tschoulte@gmail.com>
2019-08-12 09:06:00 -0500

    Working on Error Log. Implemented plus button

--- clicker.py
+++ clicker.py
@@ -3,49 +3,100 @@ import logging
 import time
 import openpyxl
 
-# logging  
-LOG = r"C:\Users\Tyler\Downloads\error.log"                                                     
-logging.basicConfig(filename=LOG, filemode="w", level=logging.DEBUG)  
+def setup_logger(location):
+    # logging                                                  
+    logging.basicConfig(filename=location, filemode="w", level=logging.DEBUG)  
 
-# console handler  
-console = logging.StreamHandler()  
-console.setLevel(logging.ERROR)  
-logging.getLogger("").addHandler(console)
+    # console handler  
+    console = logging.StreamHandler()  
+    console.setLevel(logging.ERROR)  
+    logging.getLogger("").addHandler(console)
 
-wb = openpyxl.load_workbook(r'/Users/tyler/Downloads/Steward Logistics.xlsx')
-ws = wb['Sheet2']
+def load_workbook(location):
+    wb = openpyxl.load_workbook(location)
+    ws = wb['Sheet2']
 
-db = []
-not_added = []
+    db = []
+    not_added = []
 
-for rownum in range(1,ws.max_row):
-    try:
-        hyperlink = ws.cell(row=rownum, column=1).hyperlink.target
-        quantity = ws.cell(row=rownum, column=2).value
+    for rownum in range(1,ws.max_row):
+        try:
+            hyperlink = ws.cell(row=rownum, column=1).hyperlink.target
+            quantity = ws.cell(row=rownum, column=2).value
 
-        #print(hyperlink)
-        #print(quantity)
-        if quantity != None:
-            db.append([hyperlink, quantity])
-        else:
-            not_added.append([hyperlink, quantity])
-    except:
-        logging.error("Did not add " + str(rownum))
+            #print(hyperlink)
+            #print(quantity)
+            if quantity != None:
+                db.append([hyperlink, quantity])
+            else:
+                not_added.append([hyperlink, quantity])
+        except:
+            logging.error("Did not add " + str(rownum))
+    
+    return db
 
-for item in db:
+def shop(db):
+    for item in db:
+        itemURL = item[0]
+        quantity = item[1]
+        failed = False
+
+        #load URL in Chrome
+        load_url(itemURL, failed)
+
+        #add to cart
+        click_add(itemURL, quantity, failed)
+            
+        #update quantity
+        click_plus(quantity, itemURL, failed)
+
+def load_url(itemURL, failed):
+    if failed == True:
+        return
+
     #load the url
     gui.tripleClick(x=300, y=100)
-    gui.typewrite(item[0])
+    gui.typewrite(itemURL)
     gui.hotkey('enter')
     time.sleep(3)
 
-    #find add to cart, click, update quantity
+def click_add(itemURL, quantity, failed):
+    if failed == True:
+        return
+
     try:
         x1, y1 = gui.locateCenterOnScreen("add.png", grayscale=False, confidence=.7, region=(0,0, 2000,1000))
-        gui.tripleClick(x=x1, y=y1, button='left')
-        gui.typewrite(str(int(item[1])))
-        gui.hotkey('enter')
+        gui.click(x=x1, y=y1, button='left')
     except:
-        logging.error("Couldn't add the following to cart: " + item[0])
+        logging.error("Add-To-Cart-Missing: \n" + itemURL + "\n" + str(quantity) )
+        failed = True
 
-        print()
+def click_plus(quantity, itemURL, failed):
+    if failed == True:
+        return
+
+    try:
+        x1, y1 = gui.locateCenterOnScreen("plus.png", grayscale=False, region=(0,0, 2000,1000))
+        gui.click(x=x1, y=y1, button='left', clicks=(quantity-1), interval=.125)
+        time.sleep(2)
+    except:
+        logging.error("Plus-Button-Missing: \n" + itemURL + "\n" + str(quantity) )
+        failed = True
+
+    if failed == True:
+        return
+    try:
+        gui.moveRel(100,100, duration=.125)
+        gui.leftClick()
+        x1, y1 = gui.locateCenterOnScreen("plus.png", grayscale=False, region=(0,0, 2000,1000))
+    except:
+        logging.error("Quantity-Wrong: \n" + itemURL + "\n" + str(quantity) )
+        failed = True
+
+
+
+
+setup_logger("error.log")
+#db = load_workbook("Steward Logistics.xlsx")
+db = [["https://grocery.walmart.com/ip/Great-Value-Deli-Style-Sliced-Pepper-Jack-Cheese-8-oz-12-count/155066735" , 13],["https://grocery.walmart.com/ip/Sam-s-Choice-Sliced-Monterey-Jack-Cheese-with-Jalapenos-Habaneros-and-Ghost-Peppers-8-Oz/146364618", 4],["https://grocery.walmart.com/ip/Great-Value-Cracker-Cuts-Pepper-Jack-Cheese-Slices-10-Oz/681794103" , 3],["https://grocery.walmart.com/ip/Wonder-Classic-White-Bread-20-oz-Loaf/37858875", 5]]
+shop(db)
